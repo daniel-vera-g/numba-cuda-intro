37eb55633461a508049075bf06b988b48d8e464d
danielVg <danielveragi@gmail.com>
2022-01-15 12:44:50 +0100

    feat: setup conda automatically and start jupyter on flag

--- run.sh
+++ run.sh
@@ -1,20 +1,26 @@
 #!/bin/sh
 
+# Be able to use conda in shell script
 shell_name=`basename $SHELL`
-
-
 eval "$(conda shell.$shell_name hook)"
 
 if ! conda env list | grep -q "^parallele"; then
   conda create --name parallele && \
     echo "Created the 'parallele' environment" && \
-    conda install numba numpy jupyter jupyter-notebook && \
-    echo "Installed numba, numpy, jupyter, and jupyter-notebook...Proceeding with notebook setup" && \
-    NUMBA_ENABLE_CUDASIM=1 jupyter-notebook
+    conda install numba numpy jupyterlab && \
+    echo "Installed numba, numpy, jupyterlab..."
     else
       echo "The 'parallele' environment already exists...Proceeding with actiavtion" && \
         conda activate parallele && \
-        echo "Activated the 'parallele' environment"  && \
-              NUMBA_ENABLE_CUDASIM=1 jupyter-notebook
+        echo "Activated the 'parallele' environment"
+fi
+
+wait
+
+# Accept option to activate jupyter notebook
+if [ "$1" = "--jupyter" ]; then
+  echo "Activating jupyter notebook" && \
+    # If no GPU availabl, set NUMBA_ENABLE_CUDASIM=1
+    jupyter-notebook ./cuda-notebook
 fi
 
